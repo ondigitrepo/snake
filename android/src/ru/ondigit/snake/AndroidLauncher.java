47942978f48d496d29e4c9291896ecf4bbacfa93
Sitnikov Roman <dev.sitnikov@gmail.com>
2019-02-01 23:56:23 +0300

    Starting develop game

---
+++ android/src/ru/ondigit/snake/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.ondigit.snake;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.ondigit.snake.SnakeGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SnakeGame(), config);
+	}
+}
